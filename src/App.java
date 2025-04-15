eca7754d605e7169e9a0ed2a63df1e0eaf81bc17
OSIsrael <israelos067@gmail.com>
2025-04-15 07:25:00 -0500

    Creacion de clases y paquetes

---
+++ src/App.java
@@ -0,0 +1,8 @@
+import controllers.MetodosBusqueda;
+
+public class App {
+    private MetodosBusqueda metodosBusqueda= new MetodosBusqueda();
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ src/controllers/MetodosBusqueda.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import views.ShowConsole;
+
+public class MetodosBusqueda {
+    private int[] arreglo;
+    private ShowConsole showConsole= new ShowConsole();
+    public int[] busquedaLineal(int [] arreglo){
+        return arreglo;
+    }
+    
+}
